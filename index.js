f9fb2cf43c289059f3b93cf7291131c9dcbcd4c8
Sankhadeep <sankhadeep@sahusoft.com>
2018-04-01 15:38:48 +0530

    chore(): adds cookieSession

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 const key = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -7,6 +10,16 @@ require('./services/passport');
 mongoose.connect(key.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [key.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -15,8 +25,11 @@ passport.use(
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
         if (existingUser) {
+          done(null, existingUser);
         } else {
-          new User({ googleId: profile.id }).save();
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
         }
       });
     }
