2708c2013b1b751ea695b567af07d2d44dec1d25
Sankhadeep <sankhadeep@sahusoft.com>
2018-05-21 19:54:00 +0530

    fix(): saga fix promise error

--- client/src/sagas/paymentSaga.js
+++ client/src/sagas/paymentSaga.js
@@ -5,7 +5,7 @@ import { handleTokenData } from '../services/index';
 
 export function* handleTokenRequest(action) {
   try {
-    const response = yield call(handleTokenData(action.payload));
+    const response = yield call(handleTokenData, action.payload);
     yield put({ type: FETCH_USER_SUCCESS, payload: response.data });
   } catch (e) {
     yield put({ type: FETCH_USER_FAILURE });
