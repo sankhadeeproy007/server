14d777f2e219b5bcb7fd9ce9142e6907cfcbba6f
Sankhadeep <sankhadeep@sahusoft.com>
2018-05-21 20:55:00 +0530

    chore(): adds heroku config

--- index.js
+++ index.js
@@ -26,5 +26,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve prod assets (main.js, main.css)
+  app.use(express.static('client/build'));
+
+  // Express will serve index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
