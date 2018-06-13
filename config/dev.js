c6cc4b308e9257039e969007cefa434716c49dc0
Sankhadeep <sankhadeep@sahusoft.com>
2018-06-13 20:59:30 +0530

    chore(): added env and gitignore files

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    '492298724113-us9cesprud94ebul1n03b7ml01g3g6s0.apps.googleusercontent.com',
+  googleClientSecret: 'k256eJLmFYCHLab7Udmvosk6',
+  mongoURI:
+    'mongodb://sankhadeeproy007:onetouchacces*2@ds223019.mlab.com:23019/emaily-dev',
+  cookieKey: 'nbkjHZXCuytawpi0djzaouhOIYdh',
+  stripePublishableKey: 'pk_test_nF6AyNYMmVi6ZrLUa54LCWke',
+  stripeSecretKey: 'sk_test_GxzwFEPsdKXaQJHNJuQBfmjo'
+};
