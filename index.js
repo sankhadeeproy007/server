bd58fcc9aa5d43e67c427700ddbffc28e396f5a3
sankhadeeproy007 <sankhadeeproy007@gmail.com>
2018-06-17 14:44:14 +0530

    chore(): adds survey model

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 
 const key = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(key.mongoURI);
@@ -25,6 +26,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve prod assets (main.js, main.css)
